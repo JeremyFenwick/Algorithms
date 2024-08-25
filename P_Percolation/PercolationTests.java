1a5733558d114f321e5bf625d015abbcb2dd927e
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-08-25 19:43:17 +1000

    Completed assignment for percolation

---
+++ P_Percolation/PercolationTests.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PercolationTest {
+    @Test
+    void percolationCreationTest() {
+        var percolation = new Percolation(3);
+        var count = percolation.numberOfOpenSites();
+        assertEquals(0, count);
+    }
+
+    @Test
+    void simplePercolationTest() {
+        var percolation = new Percolation(3);
+        percolation.open(1, 2);
+        assertFalse(percolation.percolates());
+        percolation.open(2, 2);
+        assertFalse(percolation.percolates());
+        percolation.open(3, 2);
+        assertTrue(percolation.percolates());
+        var count = percolation.numberOfOpenSites();
+        assertEquals(3, count);
+    }
+
+    @Test
+    void openTest() {
+        var percolation = new Percolation(3);
+        assertFalse(percolation.isFull(1, 1));
+        percolation.open(1, 1);
+        assertTrue(percolation.isOpen(1, 1));
+    }
+
+    @Test
+    void singleton() {
+        var percolation = new Percolation(1);
+        assertFalse(percolation.percolates());
+        percolation.open(1,1);
+        assertTrue(percolation.percolates());
+    }
+
+    @Test
+    void twofer() {
+        var percolation = new Percolation(2);
+        assertFalse(percolation.percolates());
+    }
+
+    @Test
+    void monteCarlo() {
+        var sim = new PercolationStats(2, 10000);
+        assertTrue(sim.mean() > 0);
+    }
+
+    @Test
+    void largeMonteCarlo() {
+        var sim = new PercolationStats(50, 20);
+        var low = sim.confidenceLo();
+        var high = sim.confidenceHi();
+        var mean = sim.mean();
+    }
+}
