585fbc654e9a769432bf229d5d75482a01a4fdc8
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-09-11 18:55:34 +1000

    More features to KdTrees

--- KDTrees/KdTree.java
+++ KDTrees/KdTree.java
@@ -1,4 +1,8 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+import java.awt.*;
+import java.util.ArrayDeque;
 
 public class KdTree {
     private Node root;
@@ -10,11 +14,60 @@ public class KdTree {
     }
 
     private static class Node {
-        public Point2D point;
+        public Point2D data;
         public boolean vertical;
         public Node left;
         public Node right;
+        public RectHV area;
+
+        public Node(Point2D point, boolean isVertical, RectHV rectangle) {
+            data = point;
+            vertical = isVertical;
+            area = rectangle;
+        }
     }
+
+    // Helper function to determine if we need to go left or right
+    private boolean goRight(Node node, Point2D point) {
+        if (node.vertical) {
+            return point.x() >= node.data.x();
+        }
+        else {
+            return point.y() >= node.data.y();
+        }
+    }
+
+    private void createNewNode(Node parent, Point2D point) {
+        // If there is no parent the tree must be empty
+        if (parent == null) {
+            root = new Node(point, true, new RectHV(0, 0, 1, 1));
+            size++;
+            return;
+        }
+        var goRight = goRight(parent, point);
+        // Right node, parent is a vertical
+        if (goRight && parent.vertical) {
+            var rectangle = new RectHV(parent.data.x(), parent.area.ymin(), parent.area.xmax(), parent.area.ymax());
+            parent.right = new Node(point, false, rectangle);
+        }
+        // Left node, parent is a vertical
+        else if (!goRight && parent.vertical) {
+            var rectangle = new RectHV(parent.area.xmin(), parent.area.ymin(), parent.data.x(), parent.area.ymax());
+            parent.left = new Node(point, false, rectangle);
+        }
+        // Right node, parent is a horizontal
+        else if (goRight) {
+            var rectangle = new RectHV(parent.area.xmin(), parent.data.y(), parent.area.xmax(), parent.area.ymax());
+            parent.right = new Node(point, true, rectangle);
+        }
+        // Left node, parent is a horizontal
+        else {
+            var rectangle = new RectHV(parent.area.xmin(), parent.area.ymin(), parent.area.xmax(),  parent.data.y());
+            parent.left = new Node(point, true, rectangle);
+        }
+        size++;
+    }
+
 
     public boolean isEmpty() {
         return root == null;
@@ -22,5 +75,67 @@ public class KdTree {
 
     public int size() {
         return size;
+    }
+
+    public void insert(Point2D newPoint) {
+        var workingNode = root;
+        Node parentNode = null;
+
+        // Identify the correct parent node for insertion
+        while (workingNode != null) {
+            // Determine if the node already exists
+            if (workingNode.data.equals(newPoint)) {
+                return;
+            }
+            // Make the working node the parent
+            parentNode = workingNode;
+            // Determine if we need to traverse left or right
+            var goRight = goRight(workingNode, newPoint);
+            if (goRight) {
+                workingNode = workingNode.right;
+            }
+            else {
+                workingNode = workingNode.left;
+            }
+        }
+
+        createNewNode(parentNode, newPoint);
+    }
+
+    public boolean contains(Point2D target) {
+        var workingNode = root;
+        while (workingNode != null) {
+            if (workingNode.data.equals(target)) {
+                return true;
+            }
+            var goRight = goRight(workingNode, target);
+            if (goRight) {
+                workingNode = workingNode.right;
+            }
+            else {
+                workingNode = workingNode.left;
+            }
+        }
+        return false;
+    }
+
+    public void draw() {
+        // Draw the lines by bfs
+        var queue = new ArrayDeque<Node>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            var node = queue.removeFirst();
+            StdDraw.setPenColor(Color.black);
+            StdDraw.setPenRadius(0.02);
+            StdDraw.point(node.data.x(), node.data.y());
+//            StdDraw.line(node.area.xmin(), node.area.ymin(), node.area.xmax(), node.area.ymax());
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
     }
 }
---
+++ KDTrees/KdTreeTest.java
@@ -0,0 +1,49 @@
+import edu.princeton.cs.algs4.Point2D;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class KdTreeTest {
+    KdTree createBasicTree() {
+        var tree = new KdTree();
+        tree.insert(new Point2D(0.7, 0.2));
+        tree.insert(new Point2D(0.5, 0.4));
+        tree.insert(new Point2D(0.2, 0.3));
+        tree.insert(new Point2D(0.4, 0.7));
+        tree.insert(new Point2D(0.9, 0.6));
+        return tree;
+    }
+
+    KdTree createUnitTree() {
+        var tree = new KdTree();
+        tree.insert(new Point2D(.5, .5));
+        return tree;
+    }
+
+    @Test
+    void treeSize() {
+        var tree = createBasicTree();
+        tree.draw();
+        assertEquals(tree.size(), 5);
+    }
+
+    @Test
+    void treeDraw() {
+        var tree = createUnitTree();
+        tree.draw();
+        assertEquals(tree.size(), 1);
+    }
+
+    @Test
+    void containsTest() {
+        var tree = createBasicTree();
+        var result = tree.contains(new Point2D(0.2, 0.3));
+        assertTrue(result);
+    }
+
+    @Test
+    void secondContainsTest() {
+        var tree = createBasicTree();
+        var result = tree.contains(new Point2D(0.1, 0.3));
+        assertFalse(result);
+    }
+}
