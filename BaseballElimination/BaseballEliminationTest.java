58153ac8a803008115fa54d48b65da80578edfe9
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2024-10-08 19:58:02 +1100

    Tested basic functionality

--- BaseballElimination/BaseballEliminationTest.java
+++ BaseballElimination/BaseballEliminationTest.java
@@ -6,6 +6,26 @@ class BaseballEliminationTest {
     @Test
     void loadTeams4() {
         var bb = new BaseballElimination("teams4.txt");
+    }
 
+    @Test
+    void atlantaWins() {
+        var bb = new BaseballElimination("teams4.txt");
+        var wins = bb.wins("Atlanta");
+        assertEquals(83, wins);
+    }
+
+    @Test
+    void philadelphiaLosses() {
+        var bb = new BaseballElimination("teams4.txt");
+        var losses = bb.losses("Philadelphia");
+        assertEquals(79, losses);
+    }
+
+    @Test
+    void montrealGamesLeft() {
+        var bb = new BaseballElimination("teams4.txt");
+        var remaining = bb.remaining("Montreal");
+        assertEquals(3, remaining);
     }
 }
